217a9c36395a380b1a46550001668f81d6651f0b
guywithpen <abreujaysonschool@gmail,com>
2018-09-06 20:27:19 -0700

    added link for first project

---
+++ javascript/sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+	createCanvas(640, 360);
+}
+
+function draw() {
+    
+    
+	background(220);
+    
+ 
+    
+    rect(310,180,140,140);
+    
+    rect(170,40,140,140);
+    
+    fill(0, 107,2,153,)
+	ellipse(320, 180, 100);
+    
+    ellipse(300,180,100);
+    
+    ellipse(310,195,100);
+    
+    fill('black');
+    ellipse(310,180,50)
+    
+}
